9edbf9f8b91227c0c770507389cc33fdaacbc3f8
foggybrains <dustin.keyes@gmail.com>
2019-09-03 10:37:23 -0600

    route

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`listenting on port ${port}!`)
 })
+
+app.get('/', (request, response) => {
+    response.send('the route worked!')
+})
