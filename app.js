0245fd40bcb8626b419eaa12629df5db50125429
foggybrains <dustin.keyes@gmail.com>
2019-09-03 20:30:23 -0600

    queries

--- app.js
+++ app.js
@@ -13,19 +13,39 @@ app.listen(port, () => {
 app.get('/instructors', (request, response) => {
     queries.listAllInstructors().then(instructors => response.send(instructors))
 })
+app.delete('/instructors/:id', (request, response) => {
+    console.log('got to delete route', request.params.id)
+    queries.deleteInstructor(request.params.id).then(response.sendStatus(204))
+})
 // students
 app.get('/students', (request, response) => {
     queries.listAllStudents().then(students => response.send(students))
+})
+app.delete('/students/:id', (request, response) => {
+    console.log('got to delete route', request.params.id)
+    queries.deleteStudent(request.params.id).then(response.sendStatus(204))
 })
 // materials
 app.get('/materials', (request, response) => {
     queries.listAllMaterials().then(materials => response.send(materials))
 })
+app.delete('/materials/:id', (request, response) => {
+    console.log('got to delete route', request.params.id)
+    queries.deleteMaterial(request.params.id).then(response.sendStatus(204))
+})
 // instructor notes
 app.get('/instructor_notes', (request, response) => {
     queries.listAllInstructorNotes().then(instructorNote => response.send(instructorNote))
 })
+app.delete('/instructor_notes/:id', (request, response) => {
+    console.log('got to delete route', request.params.id)
+    queries.deleteIntructorNote(request.params.id).then(response.sendStatus(204))
+})
 // student notes
 app.get('/student_notes', (request, response) => {
     queries.listAllStudentNotes().then(studentNote => response.send(studentNote))
+})
+app.delete('/student_notes/:id', (request, response) => {
+    console.log('got to delete route', request.params.id)
+    queries.deleteStudentNote(request.params.id).then(response.sendStatus(204))
 })
--- queries.js
+++ queries.js
@@ -5,20 +5,45 @@ module.exports = {
     listAllInstructors(){
         return database('instructors')
     },
+    deleteInstructor(id){
+        return database('instructors')
+          .where('id', id)
+          .delete()
+    },
     // students
     listAllStudents(){
         return database('students')
+    },
+    deleteStudent(id){
+        return database('students')
+          .where('id', id)
+          .delete()
     },
     // materials
     listAllMaterials(){
         return database('materials')
     },
+    deleteMaterial(id){
+        return database('materials')
+          .where('id', id)
+          .delete()
+    },
     // instructor notes
     listAllInstructorNotes(){
         return database('instructor_notes')
     },
+    deleteInstructorNote(id){
+        return database('instructor_notes')
+          .where('id', id)
+          .delete()
+    },
     // student notes
     listAllStudentNotes(){
         return database('student_notes')
+    },
+    deleteStudentNote(id){
+        return database('student_notes')
+          .where('id', id)
+          .delete()
     },
 }
